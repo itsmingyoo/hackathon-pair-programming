ae390f9d88d6b14f31f40e303d888ccd384abe39
Minh Tran <minhttran93@gmail.com>
2024-01-20 13:16:15 -0800

    agora manager

---
+++ client/src/AgoraManager/config.ts
@@ -0,0 +1,45 @@
+import { EncryptionMode, UID, SDK_MODE } from 'agora-rtc-sdk-ng';
+
+const config: configType = {
+    uid: 0,
+    appId: '',
+    channelName: 'test',
+    rtcToken: '',
+    serverUrl: '',
+    proxyUrl: 'http://localhost:8080/',
+    tokenExpiryTime: 600,
+    token: '',
+    encryptionMode: 'aes-128-gcm2',
+    salt: '',
+    encryptionKey: '',
+    destChannelName: '',
+    destChannelToken: '',
+    destUID: 2,
+    secondChannel: '',
+    secondChannelToken: '',
+    secondChannelUID: 2,
+    selectedProduct: 'rtc',
+};
+
+export type configType = {
+    uid: UID;
+    appId: string;
+    channelName: string;
+    rtcToken: string | null;
+    serverUrl: string;
+    proxyUrl: string;
+    tokenExpiryTime: number;
+    token: string;
+    encryptionMode: EncryptionMode;
+    salt: '';
+    encryptionKey: string;
+    destUID: number;
+    destChannelName: string;
+    destChannelToken: string;
+    secondChannel: string;
+    secondChannelToken: string;
+    secondChannelUID: number;
+    selectedProduct: SDK_MODE;
+};
+
+export default config;
