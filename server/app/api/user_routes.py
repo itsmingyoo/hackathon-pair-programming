e13f0fcb85ebb1896ec3e17c5523b0b1b281e622
anwersaad0 <anwer.saad0@gmail.com>
2024-01-21 13:21:24 -0800

    added follow function to user routes

--- server/app/api/user_routes.py
+++ server/app/api/user_routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import User
+from app.models import User, follow, db
+
+from sqlalchemy import select
 
 user_routes = Blueprint('users', __name__)
 
@@ -23,3 +25,31 @@ def user(id):
     """
     user = User.query.get(id)
     return user.to_dict()
+
+
+@user_routes.route('<int:follow_id>/follow/<int:user_id>', methods=['POST'])
+@login_required
+def follow_user(follow_id, user_id):
+
+    user = User.query.get(user_id)
+    following_user = User.query.get(follow_id)
+
+    if not following_user or not user:
+        return {'error': 'User not found'}
+    
+    query = select([follow]).where(
+        (follow.c.followed_user == follow_id) & (follow.c.follower == user_id)
+    )
+
+    res = db.session.execute(query)
+
+    in_follows = res.fetchone() is not None
+
+    if in_follows:
+        user.follows.remove(following_user)
+        db.session.commit()
+        return following_user.to_dict()
+    else:
+        user.follows.append(following_user)
+        db.session.commit()
+        return following_user.to_dict()
