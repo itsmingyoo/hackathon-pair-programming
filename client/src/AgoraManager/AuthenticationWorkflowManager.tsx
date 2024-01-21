b1aea0a572de694672ab939a84c224843cdeaab3
Minh Tran <minhttran93@gmail.com>
2024-01-20 19:54:36 -0800

    setup utility functions for authenticationworkflowmanager aka renewing and fetching a token

--- client/src/AgoraManager/AuthenticationWorkflowManager.tsx
+++ client/src/AgoraManager/AuthenticationWorkflowManager.tsx
@@ -1,55 +1,18 @@
 import { useEffect, useState } from 'react';
-import { useClientEvent, useRTCClient } from 'agora-rtc-react';
 import { AgoraManager } from './agoraManager';
 import config from './config';
-
-// Fetch Token from Server - Use an HTTP request to retrieve an authentication token for a specific channel from the token server.
-async function fetchRTCToken(channelName: string) {
-    if (config.serverUrl !== '') {
-        try {
-            // /rtc/:channelName/:role/:tokenType/:rtcuid/
-            const response = await fetch(
-                `${config.proxyUrl}${config.serverUrl}/rtc/${channelName}/publisher/uid/${config.uid}/?expiry=${config.tokenExpiryTime}`
-            );
-
-            const data = (await response.json()) as { rtcToken: string };
-            console.log('RTC token fetched from server: ', data.rtcToken);
-            return data.rtcToken;
-        } catch (error) {
-            console.error(error);
-            throw error;
-        }
-    } else {
-        return config.rtcToken;
-    }
-}
-
-// A token expires either after the expire time specified in the call to the token server or, if the time is not specified, after 24 hours. When the token is about to expire your app receives an event notification. To continue the current session, fetch a new token and call renewToken.
-const useTokenWillExpire = () => {
-    const agoraEngine = useRTCClient();
-    useClientEvent(agoraEngine, 'token-privilege-will-expire', () => {
-        if (config.serverUrl !== '') {
-            fetchRTCToken(config.channelName)
-                .then((token) => {
-                    console.log('RTC token fetched from server: ', token);
-                    if (token) return agoraEngine.renewToken(token);
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-        } else {
-            console.log('Please make sure you specified the token server URL in the configuration file');
-        }
-    });
-};
+import { fetchRTCToken } from '../utility/fetchRTCToken';
+import { useTokenWillExpire } from '../utility/renewRTCToken';
 
 // Retrieve a token from the token server and use it to join the channel.
 function AuthenticationWorkflowManager(props: { children?: React.ReactNode }) {
     const [channelName, setChannelName] = useState<string>('');
     const [joined, setJoined] = useState(false);
 
+    // renew token
     useTokenWillExpire();
 
+    // fetch token for channel
     const fetchTokenFunction = async () => {
         if (config.serverUrl !== '' && channelName !== '') {
             try {
---
+++ client/src/utility/renewRTCToken.ts
@@ -0,0 +1,22 @@
+import { useClientEvent, useRTCClient } from 'agora-rtc-react';
+import config from '../AgoraManager/config';
+import { fetchRTCToken } from './fetchRTCToken';
+
+// A token expires either after the expire time specified in the call to the token server or, if the time is not specified, after 24 hours. When the token is about to expire your app receives an event notification. To continue the current session, fetch a new token and call renewToken.
+export const useTokenWillExpire = () => {
+    const agoraEngine = useRTCClient();
+    useClientEvent(agoraEngine, 'token-privilege-will-expire', () => {
+        if (config.serverUrl !== '') {
+            fetchRTCToken(config.channelName)
+                .then((token) => {
+                    console.log('RTC token fetched from server: ', token);
+                    if (token) return agoraEngine.renewToken(token);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        } else {
+            console.log('Please make sure you specified the token server URL in the configuration file');
+        }
+    });
+};
