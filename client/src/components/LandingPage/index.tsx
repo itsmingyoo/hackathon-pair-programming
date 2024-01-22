ee3e4eaa545a1faebce7de0bfa38c75ff37a9c53
Jonathan Wofford <jonathanbrodie91@gmail.com>
2024-01-22 20:41:01 +0100

    splash page is styled

--- client/src/components/LandingPage/index.tsx
+++ client/src/components/LandingPage/index.tsx
@@ -5,16 +5,23 @@ import "./landingPage.css"
 function LandingPage() {
   return (
     <>
-    <div className="landing-page">
-      <div className="landing-page-content">
-        <div>
-          <div className="landing-page-cool-image">This will be a cool image</div>
-        </div>
-        <div>
-          <p className="landing-page-description">This will be a couples lines of description</p>
+      <div className="landing-page">
+        <div className="landing-page-content">
+          <div>
+            <div className="landing-page-cool-image">
+              This will be a cool image
+            </div>
+          </div>
+          <div>
+            <div className="landing-page-description">
+              <div>This will be a couples lines of description</div>
+              <div>This will be the second line</div>
+              <div>and possibly the last</div>
+              <button>Get Started</button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
