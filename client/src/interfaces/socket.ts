dcd63e06b989da78186ba0ae06b676db7cc7e664
Melinda Cortez <meli.matrix@outlook.com>
2024-01-21 22:27:48 -0800

    Create store slice for temporary chat logs for paired users

--- client/src/interfaces/socket.ts
+++ client/src/interfaces/socket.ts
@@ -1,19 +1,24 @@
 interface UserDict {
-    id: string;
-    username: string;
-    email: string;
+  id: string;
+  username: string;
+  email: string;
+}
+
+export interface TempChatMessage {
+  from: UserDict;
+  message: string;
+  created_at: string;
 }
 
 // Type for information received from the server
 export interface ServerToClientEvents {
-    joined: (data: { user: UserDict; room: string}) => void;
-    user_left: (data: string) => void;
-    temp_message_received: (data: {from: UserDict, message: string, created_at: string }) => void;
-
+  joined: (data: { user: UserDict; room: string }) => void;
+  user_left: (data: string) => void;
+  temp_message_received: (data: TempChatMessage) => void;
 }
 
 // Type for information sent to the server
 export interface ClientToServerEvents {
-    join_room: () => void;
-    leave_room: (data: {room: string}) => void;
+  join_room: () => void;
+  leave_room: (data: { room: string }) => void;
 }
--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import sessionReducer from "./session";
+import pairedChatLogReducer from "./pairedChatLog";
 
 const store = configureStore({
   // configure store with the reducer here, toolkit should come with redux-thunk middlewaree by default
   reducer: {
     session: sessionReducer,
+    pairedChatLog: pairedChatLogReducer,
   },
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware(); // Thunk middleware is included by default
