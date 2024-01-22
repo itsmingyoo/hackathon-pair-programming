db440d9102b852f7112fc670d34b946aa330767b
Minh Tran <minhttran93@gmail.com>
2024-01-22 15:14:55 -0800

    add javascript test option which handles the logic in the server as well

--- client/src/components/CodeMirror/index.tsx
+++ client/src/components/CodeMirror/index.tsx
@@ -1,6 +1,7 @@
 import { useState, useCallback, MouseEventHandler, useEffect } from 'react';
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
+import { python } from '@codemirror/lang-python';
 import { dracula } from '@uiw/codemirror-theme-dracula';
 import { FetchRoutes, fetchTestResults } from '../../utility/fetchTestResults';
 import './index.css';
@@ -21,6 +22,7 @@ function IDE(props: Props) {
     const { problemId, problemTitle } = props;
     const [value, setValue] = useState<string>('# Your Python Code Here');
     const [userResults, setUserResults] = useState<boolean[]>([]);
+    const [language, setLanguage] = useState<string>('python');
 
     const onChange = useCallback((val: string) => {
         console.log('val:', val);
@@ -29,7 +31,7 @@ function IDE(props: Props) {
 
     const handleClick: MouseEventHandler<HTMLButtonElement> = async (e) => {
         e.preventDefault();
-        const results = await fetchTestResults(value, problemId);
+        const results = await fetchTestResults(value, problemId, language);
         console.log('Finished Fetching...', results);
 
         // Handles All Edge Cases if there are errors, then return the state as false for correct rendering of elements
@@ -50,20 +52,39 @@ function IDE(props: Props) {
 
     useEffect(() => {
         console.log('😁😁😁 state', userResults);
+        console.log('😁😁😁 state', language);
     });
+
+    const handlePythonButton: MouseEventHandler<HTMLButtonElement> = (e) => {
+        e.preventDefault();
+        setLanguage('python');
+        setValue('# Your Python Code Here');
+    };
+    const handleJavascriptButton: MouseEventHandler<HTMLButtonElement> = (e) => {
+        e.preventDefault();
+        setLanguage('javascript');
+        setValue('// Your JavaScript Code Here');
+    };
 
     return (
         <>
             <h1>Problem: {problemId === undefined ? 'Please enter a problem name for props' : `${problemTitle}`}</h1>
-            <div id="user-results">
-                {userResults && userResults[0] === true ? <div>✔ Test Case 1</div> : <div>❌ Test Case 1</div>}
-                {userResults && userResults[1] === true ? <div>✔ Test Case 2</div> : <div>❌ Test Case 2</div>}
-                {userResults && userResults[2] === true ? <div>✔ Test Case 3</div> : <div>❌ Test Case 3</div>}
+            <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '5px' }}>
+                <div id="user-results">
+                    {userResults && userResults[0] === true ? <div>✔ Test Case 1</div> : <div>❌ Test Case 1</div>}
+                    {userResults && userResults[1] === true ? <div>✔ Test Case 2</div> : <div>❌ Test Case 2</div>}
+                    {userResults && userResults[2] === true ? <div>✔ Test Case 3</div> : <div>❌ Test Case 3</div>}
+                </div>
+                <div style={{ display: 'flex', gap: '5px' }}>
+                    <div>Language: </div>
+                    <button onClick={handlePythonButton}>Python</button>
+                    <button onClick={handleJavascriptButton}>JavaScript</button>
+                </div>
             </div>
             <CodeMirror
                 value={value}
                 height="400px"
-                extensions={[javascript({ jsx: true })]}
+                extensions={language === 'python' ? [python()] : [javascript({ jsx: true })]}
                 onChange={onChange}
                 theme={dracula}
             />
