2c985e32333105e0c5b51b480a717043e8b6aa79
anwersaad0 <anwer.saad0@gmail.com>
2024-01-24 21:12:28 -0800

    fixed naming for follow routes, started file for chatroom routes, removed unecessary model

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -37,7 +37,7 @@ app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(video_routes, url_prefix='/api/video')
 app.register_blueprint(code_testing_routes, url_prefix='/api/problem')
-app.register_blueprint(follow_routes, url_prefix='/api/follow')
+app.register_blueprint(follow_routes, url_prefix='/api/follows')
 db.init_app(app)
 Migrate(app, db)
 
