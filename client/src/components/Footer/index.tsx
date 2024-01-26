d2865f2e5ae5f9e2d6342dcf0c187e9633d58ec5
Jonathan Wofford <jonathanbrodie91@gmail.com>
2024-01-26 15:35:56 +0100

    added more styling to footer

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -4,68 +4,92 @@ import './Footer.css';
 
 function Footer() {
     return (
-        <div className="footer-container">
-            <div className="developer-container">
-                <div className="developer-name">Minh Tran</div>
-                <div className="developer-links-container">
-                    <div className="developer-github">
-                        <a className='github' href="https://github.com/itsmingyoo" target="_blank">
-                            GitHub
-                        </a>
-                    </div>
-                    <div className="developer-linkedin">
-                        <a className='linkedin' href="https://www.linkedin.com/in/minh-tran-36501a251" target="_blank">
-                            LinkedIn
-                        </a>
-                    </div>
-                </div>
+      <div className="footer-container">
+        <div className="all-developers">
+          <div className="developer-container">
+            <div className="developer-name">Minh Tran</div>
+            <div className="developer-links-container">
+              <div className="developer-github">
+                <a
+                  className="github"
+                  href="https://github.com/itsmingyoo"
+                  target="_blank"
+                >
+                  GitHub
+                </a>
+              </div>
+              <div className="developer-linkedin">
+                <a
+                  className="linkedin"
+                  href="https://www.linkedin.com/in/minh-tran-36501a251"
+                  target="_blank"
+                >
+                  LinkedIn
+                </a>
+              </div>
             </div>
-            <div className="developer-container">
-                <div className="developer-name">John Wofford</div>
-                <div className="developer-links-container">
-                    <div className="developer-github">
-                        <a className='github' href="https://github.com/jwoff1991" target="_blank">
-                            GitHub
-                        </a>
-                    </div>
-                    <div className="developer-linkedin">
-                        <a className='linkedin' href="https://www.linkedin.com/in/jonathanbwofford/" target="_blank">
-                            LinkedIn
-                        </a>
-                    </div>
-                </div>
+          </div>
+          <div className="developer-container">
+            <div className="developer-name">John Wofford</div>
+            <div className="developer-links-container">
+              <div className="developer-github">
+                <a
+                  className="github"
+                  href="https://github.com/jwoff1991"
+                  target="_blank"
+                >
+                  GitHub
+                </a>
+              </div>
+              <div className="developer-linkedin">
+                <a
+                  className="linkedin"
+                  href="https://www.linkedin.com/in/jonathanbwofford/"
+                  target="_blank"
+                >
+                  LinkedIn
+                </a>
+              </div>
             </div>
-            <div className="developer-container">
-                <div className="developer-name">Melinda Cortez</div>
-                <div className="developer-links-container">
-                    <div className="developer-github">
-                        <a className='github' href="#" target="_blank">
-                            GitHub
-                        </a>
-                    </div>
-                    <div className="developer-linkedin">
-                        <a className='linkedin' href="#" target="_blank">
-                            LinkedIn
-                        </a>
-                    </div>
-                </div>
+          </div>
+          <div className="developer-container">
+            <div className="developer-name">Melinda Cortez</div>
+            <div className="developer-links-container">
+              <div className="developer-github">
+                <a className="github" href="#" target="_blank">
+                  GitHub
+                </a>
+              </div>
+              <div className="developer-linkedin">
+                <a className="linkedin" href="#" target="_blank">
+                  LinkedIn
+                </a>
+              </div>
             </div>
-            <div className="developer-container">
-                <div className="developer-name">Saad Anwer</div>
-                <div className="developer-links-container">
-                    <div className="developer-github">
-                        <a className='github' href="#" target="_blank">
-                            GitHub
-                        </a>
-                    </div>
-                    <div className="developer-linkedin">
-                        <a className='linkedin' href="#" target="_blank">
-                            LinkedIn
-                        </a>
-                    </div>
-                </div>
+          </div>
+          <div className="developer-container">
+            <div className="developer-name">Saad Anwer</div>
+            <div className="developer-links-container">
+              <div className="developer-github">
+                <a className="github" href="#" target="_blank">
+                  GitHub
+                </a>
+              </div>
+              <div className="developer-linkedin">
+                <a className="linkedin" href="#" target="_blank">
+                  LinkedIn
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='copywrite'>
+            Developed for App Academy Winter Hackathon 2024
+            <div>
+                "Providing accessable websites to ensure access for everyone"
             </div>
         </div>
+      </div>
     );
 }
 
