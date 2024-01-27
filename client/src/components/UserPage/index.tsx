d641e0160a3532d53ecda0c3c5e6929f80314a53
anwersaad0 <anwer.saad0@gmail.com>
2024-01-27 12:31:00 -0800

    created file for user page

---
+++ client/src/components/UserPage/index.tsx
@@ -0,0 +1,34 @@
+import { useSelector } from 'react-redux';
+import { useAppDispatch } from '../../hooks';
+import { Navigate } from 'react-router-dom';
+import { RootState } from '../../store';
+
+
+function UserPage() {
+    const dispatch = useAppDispatch();
+
+    return (
+        <>
+            <div className='user-page'>
+                <div className='user-page-content'>
+                    <div className='user-header'>
+                        <div className='user-pic-div'>
+                            <img src="iMadeItUp" alt="bruh"></img>
+                            Placeholder until I get AWS going
+                        </div>
+
+                        <div className='user-name-div'>
+                            Put username here
+                        </div>
+                    </div>
+
+                    <div className='user-body'>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default UserPage;
