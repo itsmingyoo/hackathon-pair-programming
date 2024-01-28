cc61f820c941ada1775a54d33a66001e0ac5a83c
Jonathan Wofford <jonathanbrodie91@gmail.com>
2024-01-28 21:27:19 +0100

    reduced code on login form page and fixed formatting for if a user already has account

--- client/src/components/SignupFormPage/index.tsx
+++ client/src/components/SignupFormPage/index.tsx
@@ -1,25 +1,22 @@
 import { useState, FormEvent } from 'react';
 import { useSelector } from 'react-redux';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import { signUp } from '../../store/session';
 import { useAppDispatch } from '../../hooks';
 import { RootState } from '../../store';
 import './SignupForm.css';
 
 function SignupFormPage() {
-    const navigate = useNavigate();
     const dispatch = useAppDispatch();
-    const sessionUser = useSelector((state: RootState) => state.session.user); // Type the state
+    const sessionUser = useSelector((state: RootState) => state.session.user);
     const [email, setEmail] = useState<string>('');
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
     const [confirmPassword, setConfirmPassword] = useState<string>('');
-    const [errors, setErrors] = useState<string[]>([]); // Assuming errors are an array of strings
+    const [errors, setErrors] = useState<string[]>([]);
 
     console.log('errors', errors);
 
-    //sessionUser is returning true even if there is no user logged in because it is returning the user object { errors: [] }
-    //so we need to check if there is a user object and if there are no errors in the user object
     let userLoggedIn: boolean = false;
     if (sessionUser && !sessionUser.errors) {
         userLoggedIn = true;
@@ -48,11 +45,6 @@ function SignupFormPage() {
         }
     };
 
-    const handleAlreadyHaveAnAccount = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        e.preventDefault;
-        navigate('/login');
-    };
-
     //possible to make erros an object istead of array?
     // const emailErrorsClass = errors.email ? "email-signup-errors" : "";
     // const firstNameErrorsClass = errors.firstname ? "email-signup-errors" : "";
@@ -61,63 +53,75 @@ function SignupFormPage() {
     // const passwordErrorsClass = errors.password ? "email-signup-errors" : "";
 
     return (
-        <div className="signup-component">
-            <div className="signup-container">
-                <form onSubmit={handleSubmit}>
-                    <h1>Sign Up</h1>
-                    <label>
-                        <div className="signup-page-input-text">Email address</div>
-                        <input
-                            className="signup-form-input"
-                            placeholder="Email"
-                            type="text"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                        />
-                    </label>
-                    <label>
-                        <div className="signup-page-input-text">Username</div>
-                        <input
-                            className="signup-form-input"
-                            placeholder="Username"
-                            type="text"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            required
-                        />
-                    </label>
-                    <label>
-                        <div className="signup-page-input-text">Password</div>
-                        <input
-                            className="signup-form-input"
-                            placeholder="Password"
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                        />
-                    </label>
-                    <label>
-                        <div className="signup-page-input-text">Confirm password</div>
-                        <input
-                            className="signup-form-input"
-                            placeholder="Confirm Password"
-                            type="password"
-                            value={confirmPassword}
-                            onChange={(e) => setConfirmPassword(e.target.value)}
-                            required
-                        />
-                    </label>
-                    <button className="signup-form-button" type="submit" style={{ marginBottom: '0' }}>
-                        Sign Up
-                    </button>
-                    <button className="signup-form-button" style={{ margin: '0' }} onClick={handleAlreadyHaveAnAccount}>
-                        Already Have an account?
-                    </button>
-                </form>
+      <div className="signup-component">
+        <div className="signup-container">
+          <form onSubmit={handleSubmit}>
+            <h1>Sign Up</h1>
+            <label>
+              <div className="signup-page-input-text">Email address</div>
+              <input
+                className="signup-form-input"
+                placeholder="Email"
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </label>
+            <label>
+              <div className="signup-page-input-text">Username</div>
+              <input
+                className="signup-form-input"
+                placeholder="Username"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </label>
+            <label>
+              <div className="signup-page-input-text">Password</div>
+              <input
+                className="signup-form-input"
+                placeholder="Password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </label>
+            <label>
+              <div className="signup-page-input-text">Confirm password</div>
+              <input
+                className="signup-form-input"
+                placeholder="Confirm Password"
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+              />
+            </label>
+            <button
+              className="signup-form-button"
+              type="submit"
+              style={{ marginBottom: "0" }}
+            >
+              Sign Up
+            </button>
+            <div className="already-have-acct">
+              Already Have an account?
+              <Link
+                to={'/login'}
+                className="signup-form-button-click-here"
+                style={{ margin: "0" }}
+
+              >
+                Login Here
+              </Link>
             </div>
+          </form>
         </div>
+      </div>
     );
 }
 
