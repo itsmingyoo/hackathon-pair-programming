faab545477209f524b2aa5106cc5253b687b4b97
anwersaad0 <anwer.saad0@gmail.com>
2024-01-31 11:14:37 -0800

    added aws files to backend

---
+++ server/app/api/aws_image_helpers.py
@@ -0,0 +1,41 @@
+import boto3
+import botocore
+import os
+import uuid
+
+BUCKET_NAME = os.environ.get("S3_IMAGE_BUCKET")
+S3_LOCATION = f"https://{BUCKET_NAME}.s3.amazonaws.com/"
+ALLOWED_IMAGE_EXTENSIONS = {"png", "jpg", "jpeg"}
+
+s3 = boto3.client(
+   "s3",
+   aws_access_key_id=os.environ.get("S3_IMAGE_KEY"),
+   aws_secret_access_key=os.environ.get("S3_IMAGE_SECRET")
+)
+
+def allowed_file(filename):
+    return "." in filename and \
+           filename.rsplit(".", 1)[1].lower() in ALLOWED_IMAGE_EXTENSIONS
+
+def get_unique_filename(filename):
+    ext = filename.rsplit(".", 1)[1].lower()
+    unique_filename = uuid.uuid4().hex
+    return f"{unique_filename}.{ext}"
+
+def upload_file_to_s3(file, acl="public-read"):
+    print(os.environ.get("S3_IMAGE_KEY"))
+    try:
+        s3.upload_fileobj(
+            file,
+            BUCKET_NAME,
+            file.filename,
+            ExtraArgs={
+                "ACL": acl,
+                "ContentType": file.content_type
+            }
+        )
+    except Exception as e:
+        # in case the our s3 upload fails
+        return {"errors": str(e)}
+
+    return {"url": f"{S3_LOCATION}{file.filename}"}
