78caeda999997444539192315dc01f0065127c73
Melinda Cortez <meli.matrix@outlook.com>
2024-02-04 02:56:25 -0800

    Style edit profile form

--- client/src/components/UserPage/editProfile.tsx
+++ client/src/components/UserPage/editProfile.tsx
@@ -100,202 +100,225 @@ function EditUserPage({ setEditMode }: EditUserProps) {
       setErrors({ ...errors });
     }
   };
+  const errorClass = Object.values(errors).length
+    ? "login-form-error login-form-input"
+    : "login-form-input";
 
   return (
     <>
       <div className="edit-user-page">
-        <div className="edit-user-page-title">Edit your Profile</div>
+        <div className="edit-user-page-title">Edit Profile</div>
 
         <form onSubmit={handleConfirmEdit} id="edit-user-profile-form">
-          <div className="edit-form">
-            <label>
-              Username
-              <input
-                className="edit-input"
-                type="text"
-                value={username}
-                onChange={(e) => {
-                  if (!username.trim()) {
-                    setErrors((prev) => {
-                      const err = { ...prev };
-                      err.username = "Username cannot be empty.";
-                      return err;
-                    });
-                  } else {
-                    setErrors((prev) => {
-                      const err = { ...prev };
-                      delete err.username;
-                      return err;
-                    });
-                  }
-                  setUsername(e.target.value);
-                }}
-                required
-              />
-              <span className="input-border"></span>
-            </label>
-          </div>
-          {errors.username ? (
-            <p className="errors">* {errors.username}</p>
-          ) : null}
-          <div className="edit-form">
-            <label>
-              Profile Picture
-              <input
-                className="edit-input"
-                type="file"
-                accept="image/*"
-                onChange={handleFileChange}
-              />
-              <span className="input-border"></span>
-            </label>
-          </div>
-          {errors.pic_url ? <p className="errors">* {errors.pic_url}</p> : null}
-          <div className="edit-form">
-            <label>
-              About
-              <input
-                className="edit-input"
-                type="text"
-                value={about}
-                maxLength={500}
-                onChange={(e) => setAbout(e.target.value)}
-                required
-              />
-              <span className="input-border"></span>
-            </label>
-          </div>
-          {errors.about ? <p className="errors">* {errors.about}</p> : null}
-          <div className="edit-form">
-            <label>
-              GitHub Link
-              <input
-                className="edit-input"
-                type="url"
-                value={linkGithub}
-                onChange={(e) => {
-                  // try to validate the url
-                  if (!urlPattern.test(linkGithub)) {
-                    setErrors((prev) => {
-                      const err = { ...prev };
-                      err.link_github = "Link must be a valid url!";
-                      return err;
-                    });
-                  } else {
-                    setErrors((prev) => {
-                      const err = { ...prev };
-                      delete err.link_github;
-                      return err;
-                    });
-                  }
-                  setLinkGithub(e.target.value);
-                }}
-                required
-              />
-              <span className="input-border"></span>
-            </label>
-          </div>
-          {errors.link_github ? (
-            <p className="errors">* {errors.link_github}</p>
-          ) : null}
+          <div id="edit-form-content-wrapper">
+            <div>
+              <div className="edit-form">
+                <label className="image-label">
+                  <div className="image-upload">
+                    {profilePic  && typeof profilePic !== 'string' ? (
+                      <p className="upload-name">{profilePic.name}</p>
+                    ) : (
+                      <>
+                        <p>Upload</p>
+                      </>
+                    )}
+                    <div className="image-dot">
+                      <p>+</p>
+                    </div>
+                  </div>
+                  <input
+                    className="image-upload-input"
+                    type="file"
+                    accept="image/*"
+                    onChange={handleFileChange}
+                  />
+                  <span className="input-border"></span>
+                </label>
+              </div>
+              {errors.pic_url ? (
+                <p className="errors">* {errors.pic_url}</p>
+              ) : null}
+              <div className="edit-form">
+                <label>
+                  Username
+                  <input
+                    className={errorClass}
+                    type="text"
+                    value={username}
+                    onChange={(e) => {
+                      if (!username.trim()) {
+                        setErrors((prev) => {
+                          const err = { ...prev };
+                          err.username = "Username cannot be empty.";
+                          return err;
+                        });
+                      } else {
+                        setErrors((prev) => {
+                          const err = { ...prev };
+                          delete err.username;
+                          return err;
+                        });
+                      }
+                      setUsername(e.target.value);
+                    }}
+                    required
+                  />
+                  <span className="input-border"></span>
+                </label>
+              </div>
+              {errors.username ? (
+                <p className="errors">* {errors.username}</p>
+              ) : null}
 
-          <div className="edit-form">
-            <label>
-              LinkedIn Link
-              <input
-                className="edit-input"
-                type="text"
-                value={linkLinkedIn}
-                onChange={(e) => {
-                  // try to validate the url
-                  if (!urlPattern.test(linkLinkedIn)) {
-                    setErrors((prev) => {
-                      const err = { ...prev };
-                      err.link_linkedin = "Link must be a valid url!";
-                      return err;
-                    });
-                  } else {
-                    setErrors((prev) => {
-                      const err = { ...prev };
-                      delete err.link_linkedin;
-                      return err;
-                    });
-                  }
-                  setLinkLinkedIn(e.target.value);
-                }}
-                required
-              />
-              <span className="input-border"></span>
-            </label>
+              <div className="edit-form">
+                <label>
+                  About
+                  <input
+                    className="edit-input"
+                    type="text"
+                    value={about}
+                    maxLength={500}
+                    onChange={(e) => setAbout(e.target.value)}
+                    required
+                  />
+                  <span className="input-border"></span>
+                </label>
+              </div>
+              {errors.about ? <p className="errors">* {errors.about}</p> : null}
+            </div>
+            <div>
+              <div className="edit-form">
+                <label>
+                  GitHub Link
+                  <input
+                    className="edit-input"
+                    type="url"
+                    value={linkGithub}
+                    onChange={(e) => {
+                      // try to validate the url
+                      if (!urlPattern.test(linkGithub)) {
+                        setErrors((prev) => {
+                          const err = { ...prev };
+                          err.link_github = "Link must be a valid url!";
+                          return err;
+                        });
+                      } else {
+                        setErrors((prev) => {
+                          const err = { ...prev };
+                          delete err.link_github;
+                          return err;
+                        });
+                      }
+                      setLinkGithub(e.target.value);
+                    }}
+                    required
+                  />
+                  <span className="input-border"></span>
+                </label>
+              </div>
+              {errors.link_github ? (
+                <p className="errors">* {errors.link_github}</p>
+              ) : null}
+
+              <div className="edit-form">
+                <label>
+                  LinkedIn Link
+                  <input
+                    className="edit-input"
+                    type="text"
+                    value={linkLinkedIn}
+                    onChange={(e) => {
+                      // try to validate the url
+                      if (!urlPattern.test(linkLinkedIn)) {
+                        setErrors((prev) => {
+                          const err = { ...prev };
+                          err.link_linkedin = "Link must be a valid url!";
+                          return err;
+                        });
+                      } else {
+                        setErrors((prev) => {
+                          const err = { ...prev };
+                          delete err.link_linkedin;
+                          return err;
+                        });
+                      }
+                      setLinkLinkedIn(e.target.value);
+                    }}
+                    required
+                  />
+                  <span className="input-border"></span>
+                </label>
+              </div>
+              {errors.link_linkedin ? (
+                <p className="errors">* {errors.link_linkedin}</p>
+              ) : null}
+              <div className="edit-form">
+                <label>
+                  Portfolio Link
+                  <input
+                    className="edit-input"
+                    type="text"
+                    value={linkPortfolio}
+                    onChange={(e) => {
+                      // try to validate the url
+                      if (!urlPattern.test(linkPortfolio)) {
+                        setErrors((prev) => {
+                          const err = { ...prev };
+                          err.link_portfolio = "Link must be a valid url!";
+                          return err;
+                        });
+                      } else {
+                        setErrors((prev) => {
+                          const err = { ...prev };
+                          delete err.link_portfolio;
+                          return err;
+                        });
+                      }
+                      setLinkPortfolio(e.target.value);
+                    }}
+                    required
+                  />
+                  <span className="input-border"></span>
+                </label>
+              </div>
+              {errors.link_portfolio ? (
+                <p className="errors">* {errors.link_portfolio}</p>
+              ) : null}
+              <div className="edit-form">
+                <label>
+                  Leetcode Link
+                  <input
+                    className="edit-input"
+                    type="text"
+                    value={linkLeetcode}
+                    onChange={(e) => {
+                      // try to validate the url
+                      if (!urlPattern.test(linkLeetcode)) {
+                        setErrors((prev) => {
+                          const err = { ...prev };
+                          err.link_leetcode = "Link must be a valid url!";
+                          return err;
+                        });
+                      } else {
+                        setErrors((prev) => {
+                          const err = { ...prev };
+                          delete err.link_leetcode;
+                          return err;
+                        });
+                      }
+                      setLinkLeetcode(e.target.value);
+                    }}
+                    required
+                  />
+                  <span className="input-border"></span>
+                </label>
+              </div>
+              {errors.link_leetcode ? (
+                <p className="errors">* {errors.link_leetcode}</p>
+              ) : null}
+            </div>
           </div>
-          {errors.link_linkedin ? (
-            <p className="errors">* {errors.link_linkedin}</p>
-          ) : null}
-          <div className="edit-form">
-            <label>
-              Portfolio Link
-              <input
-                className="edit-input"
-                type="text"
-                value={linkPortfolio}
-                onChange={(e) => {
-                  // try to validate the url
-                  if (!urlPattern.test(linkPortfolio)) {
-                    setErrors((prev) => {
-                      const err = { ...prev };
-                      err.link_portfolio = "Link must be a valid url!";
-                      return err;
-                    });
-                  } else {
-                    setErrors((prev) => {
-                      const err = { ...prev };
-                      delete err.link_portfolio;
-                      return err;
-                    });
-                  }
-                  setLinkPortfolio(e.target.value);
-                }}
-                required
-              />
-              <span className="input-border"></span>
-            </label>
-          </div>
-          {errors.link_portfolio ? (
-            <p className="errors">* {errors.link_portfolio}</p>
-          ) : null}
-          <div className="edit-form">
-            <label>
-              Leetcode Link
-              <input
-                className="edit-input"
-                type="text"
-                value={linkLeetcode}
-                onChange={(e) => {
-                  // try to validate the url
-                  if (!urlPattern.test(linkLeetcode)) {
-                    setErrors((prev) => {
-                      const err = { ...prev };
-                      err.link_leetcode = "Link must be a valid url!";
-                      return err;
-                    });
-                  } else {
-                    setErrors((prev) => {
-                      const err = { ...prev };
-                      delete err.link_leetcode;
-                      return err;
-                    });
-                  }
-                  setLinkLeetcode(e.target.value);
-                }}
-                required
-              />
-              <span className="input-border"></span>
-            </label>
-          </div>
-          {errors.link_leetcode ? (
-            <p className="errors">* {errors.link_leetcode}</p>
-          ) : null}
-          <button id="edit-button" type="submit">
+          <button className="profile-buttons" type="submit">
             Submit Changes
           </button>
         </form>
